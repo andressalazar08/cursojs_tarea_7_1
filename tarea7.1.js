f506f61fd29cecbb36267f532d6b9a3567da1968
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2022-12-13 07:43:01 -0500

    tarea7

---
+++ tarea7.1.js
@@ -0,0 +1,20 @@
+/*Crea un archivo llamado conjuntos.js que contenga las siguientes líneas
+
+- Un nuevo Set con los nombres de tu familia
+
+- Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+
+- Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)*/
+
+lista = ["Joaquin", "Ruty", "Ivan","Andrés", "Juan"];
+const familia = new Set(lista);
+
+familia.add("Andrés")
+console.log(familia);
+
+familia.add("Javascript");
+console.log(familia);
+
+
+
+
